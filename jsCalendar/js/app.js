169c186f20d852ba2892f453612a787c5c223d31
Vladislav Narozni <62281680+JamesEST@users.noreply.github.com>
2020-12-07 11:37:57 +0200

    Add files via upload

---
+++ jsCalendar/js/app.js
@@ -0,0 +1,35 @@
+const button = document.querySelector('.btn-start');
+const container = document.querySelector('.container');
+const months = 24;
+
+const openDoor = (path, event) => {
+    event.target.parentNode.style.backgroundImage = `url("${path}")`;
+    event.target.style.backgroundColor = "#333";
+    event.target.style.opacity = 0;
+}
+
+const createCalendar = () => {
+    console.log('log');
+
+    for(let i = 0; i < months; i++){
+        const calendarDoor = document.createElement('div')
+        calendarDoor.classList.add('img');
+        calendarDoor.style.gridArea = "door" + (i + 1);
+        container.appendChild(calendarDoor);
+
+        const calendarDoorText = document.createElement('div')
+        calendarDoorText.classList.add('text')
+        calendarDoorText.innerHTML = i + 1;
+        calendarDoor.appendChild(calendarDoorText);
+        
+        let picNumber = i + 1;
+        let picPach = `./img/pic${picNumber}.jpg`;
+        calendarDoorText.addEventListener('click', openDoor.bind(null, picPach));
+    }
+}
+
+button.addEventListener('click', createCalendar);
+
+
+
+
